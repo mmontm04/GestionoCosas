2f6a2f8f9423c74c3cbb7bf19593a1171972190f
pvidar00 <pvidar00@estudiantes.unileon.es>
2026-01-07 21:14:51 +0100

    Delete Code directory

--- Code/src/util/patterns/observer/IStockSubject.java
+++
@@ -1,7 +0,0 @@
-package util.patterns.observer;
-
-public interface IStockSubject {
-    void attach(IStockObserver observer);
-    void detach(IStockObserver observer);
-    void notifyObservers(String nombre, double actual, double minimo);
-}
